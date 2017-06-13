e268ee1999a47e08aba3b142ff605eddce8aecf4
alexcotton <cot15011@byui.edu>
2017-06-12 18:54:17 -0700

    04 ball

--- js.js
+++ js.js
@@ -3,9 +3,57 @@
 
 
 
+/* SECTION 04 *****************************************************************/
 
+function jsonParse(url, divId, isJSON) {
+    var i = document.getElementById("sliderRange").value;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange =
+            function () {
+                if (xmlhttp.readyState === 4 && xmlhttp.status === 200)
+                    parseDataFunc(xmlhttp.responseText, divId, isJSON, i);
 
+            };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+function parseDataFunc(response, divId, isJSON, i) {
+    var responseText = (isJSON) ? JSON.parse(response) : response;
+    document.getElementById(divId).innerHTML =
+            "<ul id='pbAJAX'><li><div><img class='pbPicAJAX' src='" + responseText[i].card + "' height='250'>"
+            + "<div class='pbInfoAJAX'> Name: " + responseText[i].name + "<br>"
+            + responseText[i].from + "<br>\n\
+                Battlefield: " + responseText[i].stats.battlefield + "<br>\n\
+                Life: " + responseText[i].stats.life + "<br>\n\
+                Spellboard: " + responseText[i].stats.spellboard + "<br>\n\
+                Ability: " + responseText[i].stats.ability + "</div></div></li>\n\
+                \n\
+                <li><div><img class='pbPicAJAX' src='" + responseText[i].unique.card + "' height='250'>"
+            + "<div class='pbInfoAJAX'>Unique Card: " + responseText[i].unique.name + "<br>"
+            + "Type: " + responseText[i].unique.type + "<br>"
+            + "Placement: " + responseText[i].unique.location + "<br></div></div></li></ul><br>";
+}
 
+function jsonString(url, divId, isJSON) {
+    var i = document.getElementById("sliderRange").value;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange =
+            function () {
+                if (xmlhttp.readyState === 4 && xmlhttp.status === 200)
+                    parseDataFunc2(xmlhttp.responseText, divId, isJSON, i);
+            };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+function parseDataFunc2(response, divId, isJSON, i) {
+    var responseText = (isJSON) ? JSON.parse(response) : response;
+    document.getElementById(divId).innerHTML = response;
+}
+
+function bounce() {
+    document.getElementById("animB").style.animationPlayState = "running";
+}
 
 
 
@@ -72,7 +120,7 @@ function slideChange() {
 }
 
 var tries = 0;
-function triesCount(){
+function triesCount() {
     tries += 1;
     document.getElementById("triesNumber").innerHTML = tries;
 }

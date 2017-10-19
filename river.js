4becfa6a1e6a2dab21f52883028725cf3cd6ce80
alexcotton <cot15011@byui.edu>
2017-10-18 19:47:23 -0700

    carrot rabbit fox - js movement and logic

--- river.js
+++ river.js
@@ -1,6 +1,7 @@
-var rabbit = 0;
-var carrot = 0;
-var fox = 0;
+var rabbit = "topDrop";
+var carrot = "topDrop";
+var fox = "topDrop";
+var boatLocation = 0;
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -13,9 +14,46 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     var targetChildCount = ev.target.childElementCount;
-    console.log (targetChildCount);
+        if (targetChildCount === 1) {
+            return false;
+        }
         if (!ev.target.getAttribute("ondrop"))
             return false;
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+}
+
+function boatCross() {
+    var carrotParentNode = document.getElementById("carrot").parentNode;
+    var carrotParentId = (carrotParentNode.id);
+    var rabbitParentNode = document.getElementById("rabbit").parentNode;
+    var rabbitParentId = (rabbitParentNode.id);
+    var foxParentNode = document.getElementById("fox").parentNode;
+    var foxParentId = (foxParentNode.id);
+    var waterHeight = document.getElementById("water").style.height;
+    console.log("carrotParentId = " + carrotParentId);
+    console.log("rabbitParentId = " + rabbitParentId);
+    console.log("foxParentId = " + foxParentId);
+    if (boatLocation === 0) {
+        if ((carrotParentId === "topDrop" && rabbitParentId === "topDrop") || (rabbitParentId === "topDrop" && foxParentId === "topDrop")) {
+           console.log("(boatLocation === 0) error");
+           return false;
+       }
+        document.getElementById("boat").style.transform = "translateY(100%)";
+        boatLocation = 1;
+        return;
+    }
+    if (boatLocation === 1) {
+        if ((carrotParentId === "bottomDrop" && rabbitParentId === "bottomDrop") || (rabbitParentId === "bottomDrop" && foxParentId === "bottomDrop")) {
+           console.log("(boatLocation === 1) error");
+           return false;
+        }
+        document.getElementById("boat").style.transform = "translateY(0%)";
+        boatLocation = 0;
+        return;
+    }
+    else {
+        console.log("function boatCross error");
+    }
+       
 }

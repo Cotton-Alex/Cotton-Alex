d02389cf71a0c38974d3ee6bb43458e1dc662d13
Alex <cot15011@byui.edu>
2017-11-14 12:42:41 -0800

    Update app.js
    updated final dice to red or blue based on activePlayer

--- Dice-Game/app.js
+++ Dice-Game/app.js
@@ -118,7 +118,7 @@ var diceBounceTimes = Math.floor(Math.random() * 3) + 3;
                 //display result
                 var diceDOM = document.querySelector('.dice');
                 diceDOM.style.display = 'block';
-                diceDOM.src = 'dice-' + dice + '.png';
+                diceDOM.src = 'player-' + activePlayer + '-dice-' + diceBounceNumber + '.png';
                 document.getElementById('diceID').style.left = (0 + (Math.random() * 60)) + "%";
                 document.getElementById('diceID').style.top = (65 + (Math.random() * 160)) + "px";
                 

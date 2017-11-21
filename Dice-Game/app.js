4b797d99e282ae4b1800157999a9aafbc4cadb43
Alex <cot15011>
2017-11-20 21:37:11 -0700

    Trying to get voice working on mobile

--- Dice-Game/app.js
+++ Dice-Game/app.js
@@ -7,6 +7,12 @@ responsiveVoice.setDefaultVoice(voice);
 var voicelist = responsiveVoice.getVoices();
 console.log(voicelist);
 
+//document.getElementById(‘#roll-0’).addEventListener("click", responsiveVoice.clickEvent);
+//document.getElementById(‘#roll-1’).addEventListener("click", responsiveVoice.clickEvent);
+
+document.querySelector('#roll-0') addEventListener("click", responsiveVoice.clickEvent);
+document.querySelector('#roll-1') addEventListener("click", responsiveVoice.clickEvent);
+
 var select = document.getElementById("voiceSelect"),
         voiceList = responsiveVoice.getVoices();
 
